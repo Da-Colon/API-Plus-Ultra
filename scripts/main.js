67bf8b6f67428f6d5e337b7d838cd10f4e118c96
Kyra Mellema <kemellema@gmail.com>
2019-10-03 15:25:47 -0400

    changed page icon and added new function

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,13 @@ document.getElementById('kyraGit').addEventListener('click', function (e) {
 document.getElementById('gilGit').addEventListener('click', function (e) {
     window.open('https://github.com/GilliamD')
     });
+
+
+
+const goku = get("https://api.themoviedb.org/3/search/tv?api_key=cc42102845a020075536de832b824222&language=en-US&query=Dragonball%20z&page=1")
+    goku.then(info => {
+        const gokuDescription = info.results[1].overview
+        console.log(gokuDescription)
+    })
+
+
